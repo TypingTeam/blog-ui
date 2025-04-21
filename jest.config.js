d26cfaa633f5b34e3b3ee788212548fae120ffbd
Sergii Lishcuk <sergii.lischuk@gmail.com>
2025-04-21 14:45:52 +0300

    Finished migration from karma to jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["./tests/setup-jest.ts"],
-  globalSetup: "jest-preset-angular/global-setup",
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,22 @@
-import { ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { screen, fireEvent } from '@testing-library/angular';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be rendered', () => {
+    expect(component).toBeTruthy();
+  });
 });
