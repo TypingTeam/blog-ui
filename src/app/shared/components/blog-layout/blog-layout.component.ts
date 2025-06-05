7174d2e7935069370a21799282178fae7ec4e96c
Sergii Lishcuk <sergii.lischuk@gmail.com>
2025-06-05 22:49:58 +0300

    Blog layout update

--- src/app/shared/components/blog-layout/blog-layout.component.ts
+++ src/app/shared/components/blog-layout/blog-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BlogLayoutFooter } from '@shared/components/blog-layout-footer/blog-layout-footer.component';
 import { BlogLayoutHeader } from '@shared/components/blog-layout-header/blog-layout-header.component';
 
@@ -6,6 +7,6 @@ import { BlogLayoutHeader } from '@shared/components/blog-layout-header/blog-lay
   selector: 'app-blog-layout',
   templateUrl: './blog-layout.component.html',
   styleUrl: './blog-layout.component.scss',
-  imports: [BlogLayoutFooter, BlogLayoutHeader],
+  imports: [BlogLayoutFooter, BlogLayoutHeader, RouterModule],
 })
 export class BlogLayout {}
