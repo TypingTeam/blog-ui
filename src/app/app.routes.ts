5a798ef39ece51cffe8a5891a5475c6298b0f378
Sergii Lishcuk <sergii.lischuk@gmail.com>
2025-06-10 22:45:31 +0300

    Minor changes in structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,30 @@
 import { Routes } from '@angular/router';
 import { BlogLayout } from '@shared/components/blog-layout/blog-layout.component';
+import { PageNotFoundComponent } from '@shared/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: '/blog',
     component: BlogLayout,
     children: [
       {
-        path: '',
+        path: 'blog',
+        loadChildren: () =>
+          import('./features/landing/landing.module').then(
+            (m) => m.LandingModule,
+          ),
+      },
+      {
+        path: 'projects',
+        loadChildren: () =>
+          import('./features/landing/landing.module').then(
+            (m) => m.LandingModule,
+          ),
+      },
+      {
+        path: 'about',
         loadChildren: () =>
           import('./features/landing/landing.module').then(
             (m) => m.LandingModule,
@@ -15,4 +32,5 @@ export const routes: Routes = [
       },
     ],
   },
+  { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { LandingComponent } from '@features/landing/components/landing/landing.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  imports: [CommonModule, LandingComponent],
 })
 export class LandingModule {}
